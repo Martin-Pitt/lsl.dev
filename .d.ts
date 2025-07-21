2d888dde99d816b04e21a2ff16f34a5aa5629e63
Martin Pitt <martin.r.pitt@gmail.com>
2025-07-21 01:17:40 +0100

    Was trying to make intellisense pick up on the definitions JSON, was unsuccessful but dropping it here

---
+++ .d.ts
@@ -0,0 +1,58 @@
+interface LSLDefinitions {
+	"llsd-lsl-syntax-version": number
+	constants: {
+		[k: string]: {
+			tooltip?: string
+			type?: string
+			value?: number | string
+			[k: string]: unknown
+		}
+	}
+	events: {
+		[k: string]: {
+			arguments?:
+			| {
+				[k: string]: {
+					tooltip?: string
+					type?: string
+					index_semantics?: boolean
+				}
+			}[]
+			tooltip?: string
+		}
+	}
+	functions: {
+		[k: string]: {
+			arguments?:
+			| {
+				[k: string]: {
+					tooltip?: string
+					type?: string
+					index_semantics?: boolean
+				}
+			}[]
+			| null
+			energy?: number
+			"func-id"?: number
+			native?: boolean
+			pure?: boolean
+			return?: string
+			sleep?: number
+			tooltip?: string
+			bool_semantics?: boolean
+			experience?: boolean
+			deprecated?: boolean
+			private?: boolean
+			index_semantics?: boolean
+			"god-mode"?: boolean
+			"linden-experience"?: boolean
+			mono_sleep?: number
+		}
+	}
+	types: {
+		tooltip?: string
+	}
+	controls: {
+		tooltip?: string
+	}
+}
